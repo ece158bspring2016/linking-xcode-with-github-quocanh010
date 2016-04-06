9be4dc0b61eed28e27f234f4b0c91125fb4f5d00
quocanh010 <quocanh010@gmail.com>
2016-04-05 21:04:08 -0700

    first commit

---
+++ hw1_final/SCview.swift
@@ -0,0 +1,25 @@
+//
+//  SCview.swift
+//  hw1_final
+//
+//  Created by Anh Mai on 4/5/16.
+//  Copyright © 2016 Anh Mai. All rights reserved.
+//
+
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
